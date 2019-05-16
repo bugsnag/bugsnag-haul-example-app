f3a435a1360eeb46170d40366255573712411451
fractalwrench <fractalwrench@gmail.com>
2019-05-16 16:13:25 +0100

    fix: perform extra setup required to get haul working with 0.59

--- haul.config.js
+++ haul.config.js
@@ -1,7 +1,25 @@
 import { createWebpackConfig } from "haul";
 
 export default {
-  webpack: createWebpackConfig(({ platform }) => ({
-    entry: `./index.js`
-  }))
+  webpack: env => {
+    const config = createWebpackConfig({
+      entry: './index.js',
+    })(env);
+
+    config.module.rules.some(rule => {
+      if (rule.test && rule.test.source.includes('js')) {
+        rule.use = [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              presets: [['module:metro-react-native-babel-preset', { enableBabelRuntime: false }]],
+              plugins: [require.resolve('haul/src/utils/fixRequireIssues')],
+            },
+          },
+        ];
+        return true;
+      }
+    });
+    return config;
+  }
 };
